179592bf8692712d3ff2da4c7dbc055174b07e82
AldinaIsmic <127144540+AldinaIsmic@users.noreply.github.com>
2023-05-30 21:54:14 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main () {
     int a, b;
-    cout<<"unesite vrijednost a "<<endl;
+    cout<<"unesite vrijednost A "<<endl;
     cin>>a;
-    cout<<"unesite vrijednost b "<<endl;
+    cout<<"unesite vrijednost B "<<endl;
     cin>>b;
     if (a>b)
-        cout<<"a je veci broj"<<endl;
+        cout<<"A je veci broj."<<endl;
         else
-            cout<<"b je veci broj"<<endl;
+            cout<<"B je veci broj."<<endl;
 
             cout<<"Hell on world :("<<endl;
       cin.ignore();
